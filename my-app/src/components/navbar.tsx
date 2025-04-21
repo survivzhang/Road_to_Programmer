7dfb2225e8831ad395a4478b93a3702865768b31
zichen <zzcnhy@gmail.com>
2025-04-21 23:22:47 +0800

    navbar work

---
+++ my-app/src/components/navbar.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Button, buttonVariants } from "@/components/ui/button";
+
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+const navItemStyle = "px-3 py-1.5 rounded-md hover:bg-[var(--accent)]";
+
+export function Navbar() {
+  return (
+    <div className="flex justify-between items-center p-2 shadow-md bg-[var(--secondary)] text-[var(--secondary-foreground)]">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild className={navItemStyle}>
+              <Link href="#">RoadMap</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild className={navItemStyle}>
+              <Link href="#">Skills</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild className={navItemStyle}>
+              <Link href="#">JobMarket</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <div className="flex flex-row items-center">
+        <div className="relative h-12 w-auto aspect-[2.5]">
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            fill
+            className="object-contain"
+          />
+        </div>
+
+        <Button asChild className={navItemStyle}>
+          <Link href="#">Login</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
